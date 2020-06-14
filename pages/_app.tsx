f568a37f7888b30b4af2394b9c6eb11e6cfcb978
Thai Pangsakulyanont <dtinth@spacet.me>
2020-06-14 10:31:35 +0700

    add apple meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,14 @@ export default function MyApp({ Component, pageProps }: any) {
       <Head>
         <link rel="icon" href="/icon.svg" />
         <link rel="manifest" href="/manifest.json" />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-startup-image"
+          sizes="2048x2732"
+          href="/apple-launch-image.png"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </Head>
       <Component {...pageProps} />
     </>
